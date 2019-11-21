9ac7a99378fb9d84017855fcfd654d665f8b5ef7
Joshua Wilborn <joshua.j.wilborn@gmail.com>
2019-11-21 18:48:37 -0500

    Initial commit

---
+++ colorize-the-logs.js
@@ -0,0 +1,44 @@
+const { colorMap, toDelete, resetChar } = window.__colorizeTheLogs;
+
+function colorize(htmlString) {
+  let str = htmlString;
+  let changed = false;
+  for (let query in colorMap) {
+    let color = colorMap[query];
+    let old = str;
+    str = str.replace(new RegExp(query, 'g'), `<span style="color: ${color}">`);
+    if (str != old) changed = true;
+  }
+  if (changed) str = str.replace(new RegExp(resetChar, 'g'), '</span>');
+  toDelete.forEach(match => (str = str.replace(new RegExp(match, 'g'), '')));
+  return str;
+}
+function colorTheLogs() {
+  console.log('🌈🌈🌈');
+  [...document.querySelectorAll('.ace_line:not(.🌈)')].map(line => {
+    let target = line.children[0];
+    target.innerHTML = colorize(target.innerText);
+    line.classList.add('🌈');
+  });
+}
+
+function startObserver() {
+  const container = document.querySelector('awsui-tabs');
+  const observer = new MutationObserver(colorTheLogs);
+  observer.observe(container, {
+    childList: true,
+    subtree: true
+  });
+  colorTheLogs();
+}
+let watchInterval;
+function watchForContainer() {
+  watchInterval = setInterval(() => {
+    if (document.querySelector('awsui-tabs')) {
+      startObserver();
+      clearInterval(watchInterval);
+    }
+  }, 50);
+}
+console.log('🌈🌈🌈 Loading... 🌈🌈🌈');
+watchForContainer();
