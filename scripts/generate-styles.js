4a3571b9a031e4f84dcbe28f6f82bf50f048c611
Joshua Wilborn <joshua.j.wilborn@gmail.com>
2019-11-24 14:58:19 -0500

    fix misnumbered grey color escape code; update css generation script

--- scripts/generate-styles.js
+++ scripts/generate-styles.js
@@ -1,3 +1,5 @@
+const path = require('path');
+const fs = require('fs');
 const colorMap = {
     "30m": '#000000',
     "31m": '#800000',
@@ -7,10 +9,8 @@ const colorMap = {
     "35m": '#800080',
     "36m": '#008080',
     "37m": '#c0c0c0',
-    "39m": '#808080'
+    "38m": '#808080'
 }
-const path = require('path');
-const fs = require('fs');
 let styles = Object.entries(colorMap).map(([colorCode, color]) => {
   return `
     .🌈-${colorCode} {
@@ -18,8 +18,4 @@ let styles = Object.entries(colorMap).map(([colorCode, color]) => {
     }
   `
 }).join('');
-fs.writeFileSync(path.join(__dirname, '../css/styles.css'), `
-  <style>
-    ${styles}
-  </style>
-`, 'utf-8');
+fs.writeFileSync(path.join(__dirname, '../css/styles.css'), styles, 'utf-8');
