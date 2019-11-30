b5eba000e173b4d2263bc7a4aba245df5168fd83
Joshua Wilborn <joshua.j.wilborn@gmail.com>
2019-11-30 13:39:37 -0500

    Add coverage reporting

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,6 @@ module.exports = {
   verbose: false,
   silent: false,
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  collectCoverage: true
 };
