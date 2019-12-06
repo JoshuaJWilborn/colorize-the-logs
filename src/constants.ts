b851a6cf64f2b77ef974668504893b39a24a785c
Joshua Wilborn <joshua.j.wilborn@gmail.com>
2019-12-05 19:54:44 -0500

    Add 24-bit True Color Support

--- src/constants.ts
+++ src/constants.ts
@@ -14,4 +14,12 @@ export const regexReplacementFunctionPairs: RegStringPair[] = [
   [/(\[)(\d{1,3})(;(\d{1,3}))?m/g, (match, p1, p2, p3, p4, p5) => {
     return `<span class="🌈-${p2}m${p4 ? " 🌈-${p4}m" : ""}">`;
   }],
+  [/\[(38|48);2;(\d{1,3});(\d{1,3});(\d{1,3})(;(38|48);2;(\d{1,3});(\d{1,3});(\d{1,3}))?m/g,
+    (match, layer1, r1, g1, b1, unused, layer2, r2, g2, b2) => {
+    let style = `${layer1 === "38" ? "" : "background-"}color: rgb(${r1},${g1},${b1});`;
+    if (layer2) {
+      style +=  `${layer2 === "38" ? "" : "background-"}color: rgb(${r2},${g2},${b2});`;
+    }
+    return `<span style="${style}">`;
+  }],
 ];
